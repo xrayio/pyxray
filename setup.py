fdbc9cef835cd24b3c127352e7c29b6d6da0df60
Kfir Gollan <kfir.gollan@gmail.com>
2018-06-22 15:37:46 +0300

    Initial preparations for uploading pyxray to PyPI.
    Based on the guide at https://packaging.python.org/tutorials/packaging-projects/

--- setup.py
+++ setup.py
@@ -6,16 +6,29 @@ from setuptools import find_packages
 # pip install -e .                                              # install from source
 # python setup.py sdist                                         # create dist
 # sudo pip install --no-index --find-links=./dist/ xray         # install dist
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='xray',
-    version='0',
+    name='pyxray',
+    version='0.1',
+    author='Gregory Freilikhman',
+    description='Python client for xrayio tools',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xrayio/pyxray",
+    scripts=['apps/monitor'],
     packages=find_packages(),
-    install_requires=[
-        'psutil',
-        'gevent', 
-	'zmq',
-    ],
     license='MIT',
-    scripts=['apps/monitor']
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'zmq',
+        'gevent',
+        'psutil',
+    ],
 )
